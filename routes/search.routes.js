f4c13bd225ccb28d64aaac48e98e50288e2d95b0
eskalona17 <eskalona_17@hotmail.com>
2019-04-28 19:21:44 +0200

    pequeños cambios y rutas de buscador

---
+++ routes/search.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const secure = require('../middlewares/secure.mid');
+const users = require('../controllers/users.controller');
+
+// router.get('/', secure.isAuthenticated, users.list);
+//router.get('/:id/search', secure.isAuthenticated, users.search);
+
+// router.post('/:id/delete', secure.isAuthenticated, secure.checkRole('admin'), users.delete);
+
+module.exports = router;
